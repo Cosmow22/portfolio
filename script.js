ea37b6c314ea5e2d3ef5c4379b38aea36b6a6660
Cosmow22 <cosmow543@gmail.com>
2026-04-11 12:37:12 +0200

    crt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var typed = new Typed('#hello', {
   strings: ["Hello World!", "Je suis Cosmow22"],
   typeSpeed: 100,
   backSpeed: 50,
-  blackDelay: 1000,
+  backDelay: 1000,
   startDelay: 500,
   loop: false
   });
