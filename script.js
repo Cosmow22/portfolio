707d58aea5621b3dedfbcf08f36143ab03a63a6a
Cosmow22 <marchalcome323@gmail.com>
2026-04-25 16:49:52 +0200

    save

--- script.js
+++ script.js
@@ -27,3 +27,6 @@ const blur_elem = document.getElementById( "blur-around" );
 document.onmousemove = (evt) => {
   blur_elem.style.transform = `translate(${evt.clientX}px, ${evt.clientY}px)`;
 };
+
+
+console.log("👀")
